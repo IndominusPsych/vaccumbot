14e263acaaf2d6af4b6613be7677aacbecd9a4cf
Aryak47 <aryakajayan12@gmail.com>
2024-06-17 18:10:39 +0200

    goal_pose fixed

--- scripts/goal_pose.py
+++ scripts/goal_pose.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Twist
+import sys
 
 # Callbacks definition
 
@@ -26,18 +27,24 @@ rospy.init_node('goal_pose')
 navclient = actionlib.SimpleActionClient('move_base',MoveBaseAction)
 navclient.wait_for_server()
 
-# Example of navigation goal
+# Navigation goal
+default_x = -1.0
+default_y = 2.0
+if len(sys.argv) > 1:
+    current_x = float(sys.argv[-2])
+    current_y = float(sys.argv[-1])
+
 goal = MoveBaseGoal()
 goal.target_pose.header.frame_id = "map"
 goal.target_pose.header.stamp = rospy.Time.now()
 
-goal.target_pose.pose.position.x = -1.0
-goal.target_pose.pose.position.y = 2.0
+goal.target_pose.pose.position.x = current_x
+goal.target_pose.pose.position.y = current_y
 goal.target_pose.pose.position.z = 0.0
 goal.target_pose.pose.orientation.x = 0.0
 goal.target_pose.pose.orientation.y = 0.0
-goal.target_pose.pose.orientation.z = 1.0
-goal.target_pose.pose.orientation.w = 0.0
+goal.target_pose.pose.orientation.z = 0.0
+goal.target_pose.pose.orientation.w = 1.0
 
 navclient.send_goal(goal, done_cb, active_cb, feedback_cb)
 finished = navclient.wait_for_result()
